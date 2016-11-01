fc389de457fb3fee38c8af26750f104ab325dc92
qc179 <179313554@qq.com>
2016-11-01 17:19:12 +0800

    升级更新代码，使能被python3解释运行，仍在调校中，未全部完成 从python2到python3，部分函数名称和使用方法有变化，部分函数的对象也变了，例如list的sort对象，和raw_input在python3中并不直接支持，可能有替代方式，还在尝试，待全部理清。

--- averaged.py
+++ averaged.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
-#filename:averaged.py
-#Auto grouped weibo's bid averaged before init
-#This tool works with weibo_bids.xls in current work file
-#weibo_bids.xls:col0 is id,col1 is number
+# -*- coding: utf-8 -*-
+# filename:averaged.py
+# Auto grouped weibo's bid averaged before init
+# This tool works with weibo_bids.xls in current work file
+# weibo_bids.xls:col0 is id,col1 is number
 
 import xlrd
 from collections import deque
@@ -12,36 +12,37 @@ getbids = xlrd.open_workbook('weibo_bids.xls')
 bids_sheet1 = getbids.sheets()[0]
 bids_list = []
 for row in range(bids_sheet1.nrows):
-    #把bid化整后字符串化
-    bid = str(int(bids_sheet1.cell(row,0).value))
-    #把文章数化整
-    artnum = int(bids_sheet1.cell(row,1).value)
-    #添加到列表
-    bids_list.append((bid,artnum))
-#转换成字典
+    # 把bid化整后字符串化
+    bid = str(int(bids_sheet1.cell(row, 0).value))
+    # 把文章数化整
+    artnum = int(bids_sheet1.cell(row, 1).value)
+    # 添加到列表
+    bids_list.append((bid, artnum))
+# 转换成字典
 bids_dict = dict(bids_list)
-print u'共有',len(bids_dict),u'个id需要分组'
+print(u'共有', len(bids_dict), u'个id需要分组')
 
 val_list = bids_dict.values()
 val_list_len = len(val_list)
 
 val_sum = sum(val_list)
-print u'\n全部id总计和为',val_sum
+print(u'\n全部id总计和为', val_sum)
 
-val_list.sort(reverse=True)
+print(val_list)
+# val_list.sort()
 val_queue = deque(val_list)
 
-print u'\n需要均分为几组：'
+print(u'\n需要均分为几组：')
 spider_num = raw_input()
 spider_num = int(spider_num)
 
-avg_target = val_sum/spider_num
-print u'\n目标均值：',avg_target,'\n'
+avg_target = val_sum / spider_num
+print(u'\n目标均值：', avg_target, '\n')
 
 group = []
 group_num = 0
 group_list = []
-while len(val_queue)>0:
+while len(val_queue) > 0:
     group_len = len(group)
     if group_len == 0:
         group_sum = 0
@@ -49,45 +50,44 @@ while len(val_queue)>0:
         group_sum = sum(group)
 
     if group_num == val_list_len:
-        print group
+        print(group)
         group_list.append(group)
-        print u'---单组合计',group_sum,u'相差均值',group_sum-avg_target
-        print u'共',group_num,u'个数值，全部完成均分'
+        print(u'---单组合计', group_sum, u'相差均值', group_sum - avg_target)
+        print(u'共', group_num, u'个数值，全部完成均分')
         break
-    elif abs(avg_target-group_sum) < min(val_queue):
-        print group
+    elif abs(avg_target - group_sum) < min(val_queue):
+        print(group)
         group_list.append(group)
-        print u'--单组合计',group_sum,u'相差均值',group_sum-avg_target
+        print(u'--单组合计', group_sum, u'相差均值', group_sum - avg_target)
         group = []
     elif group_sum >= avg_target:
-        print group
+        print(group)
         group_list.append(group)
-        print u'-单组合计',group_sum,u'相差均值',group_sum-avg_target
+        print(u'-单组合计', group_sum, u'相差均值', group_sum - avg_target)
         group = []
-
 
     if len(val_queue) == 1:
         cur = val_queue.popleft()
         group.append(cur)
         group_num += 1
-        print group
+        print(group)
         group_list.append(group)
-        print u'---单组合计',sum(group),u'相差均值',sum(group)-avg_target
-        print u'\n共',group_num,u'个数值，全部完成均分'
+        print(u'---单组合计', sum(group), u'相差均值', sum(group) - avg_target)
+        print(u'\n共', group_num, u'个数值，全部完成均分')
         break
     else:
         cur = val_queue.popleft()
         if cur >= avg_target:
             group.append(cur)
             group_num += 1
-            print group
+            print(group)
             group_list.append(group)
-            print u'大于均值 相差均值',sum(group)-avg_target
+            print(u'大于均值 相差均值', sum(group) - avg_target)
             group = []
             continue
         else:
-            curx = abs(avg_target-group_sum-cur)
-            oxlist = [abs(avg_target-group_sum-o) for o in val_queue]
+            curx = abs(avg_target - group_sum - cur)
+            oxlist = [abs(avg_target - group_sum - o) for o in val_queue]
             ox = min(oxlist)
             if curx <= ox:
                 group.append(cur)
@@ -95,10 +95,10 @@ while len(val_queue)>0:
             else:
                 val_queue.append(cur)
 
-print u'\n----以下为对应id的分组----\n'
+print(u'\n----以下为对应id的分组----\n')
 bids_list = deque(bids_list)
 
-wresult = open('result.txt','w')
+wresult = open('result.txt', 'w')
 
 for gl in group_list:
     bid_group = []
@@ -110,11 +110,11 @@ for gl in group_list:
             else:
                 bid_group.append(int(bidque[0]))
                 break
-    print bid_group
+    print(bid_group)
     wresult.write(str(bid_group))
     wresult.write('\n')
 
 wresult.close()
 
-print '*'*75
+print('*' * 75)
 anyenter = raw_input('Grouped completed.Press Enter to quit.')
