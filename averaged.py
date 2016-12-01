5b43556db8f458a375c4d5f5176e67eaafb50938
qc179 <179313554@qq.com>
2016-12-01 17:06:17 +0800

    refactor 未完成的重构 准备重新调整实现逻辑，这次未完成，功能没有变化，暂存

--- averaged.py
+++ averaged.py
@@ -1,121 +1,120 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # filename:averaged.py
-# Auto grouped weibo's bid averaged before init
-# This tool works with weibo_bids.xls in current work file
-# weibo_bids.xls:col0 is id,col1 is number
 
 import xlrd
 import sys
+import time
 from collections import deque
 
-getbids = xlrd.open_workbook('weibo_bids.xls')
-bids_sheet1 = getbids.sheets()[0]
-bids_list = []
-for row in range(bids_sheet1.nrows):
-    # 把bid化整后字符串化
-    bid = str(int(bids_sheet1.cell(row, 0).value))
+id_val_xls = xlrd.open_workbook('weibo_bids.xls')
+id_val_sheet1 = id_val_xls.sheets()[0]
+id_val_list = []
+for row in range(id_val_sheet1.nrows):
+    # 把id化整后字符串化
+    id = str(int(id_val_sheet1.cell(row, 0).value))
     # 把文章数化整
-    artnum = int(bids_sheet1.cell(row, 1).value)
+    artnum = int(id_val_sheet1.cell(row, 1).value)
     # 添加到列表
-    bids_list.append((bid, artnum))
+    id_val_list.append((id, artnum))
+
 # 转换成字典
-bids_dict = dict(bids_list)
-print(u'共有', len(bids_dict), u'个id需要分组')
-
-val_list = list(bids_dict.values())
+id_val_dict = dict(id_val_list)
+val_list = list(id_val_dict.values())
 val_list_len = len(val_list)
-
-val_sum = sum(val_list)
-print(u'\n全部id总计和为', val_sum)
-
-print(val_list)
+val_list_sum = sum(val_list)
 val_list.sort(reverse=True)
-val_queue = deque(val_list)
+val_list_queue = deque(val_list)
+print('读取到以下这些值：')
+print(val_list)
+print('*' * 79)
 
-print(u'\n需要均分为几组：')
-spider_num = int(input())
+print('共有', len(id_val_dict), '个值需要分组，总和等于', val_list_sum)
+spider_num = int(input('需要均分为几组，请输入：'))
+avg_target = int(val_list_sum / spider_num)
+print('将以每组值合计约等于', avg_target, '为目标对所有值进行分组')
+time.sleep(1)
+print('开始分组...\n')
+time.sleep(1)
 
-avg_target = val_sum / spider_num
-print(u'\n目标均值：', avg_target, '\n')
-
-group = []
-group_num = 0
+ready_group = []
+ready_group_num = 0
 group_list = []
-while len(val_queue) > 0:
-    group_len = len(group)
-    if group_len == 0:
-        group_sum = 0
+while len(val_list_queue) > 0:
+    ready_group_num_0 = len(ready_group)
+    if ready_group_num_0 == 0:
+        ready_group_sum = 0
     else:
-        group_sum = sum(group)
+        ready_group_sum = sum(ready_group)
 
-    if group_num == val_list_len:
-        print(group)
-        group_list.append(group)
-        print(u'---单组合计', group_sum, u'相差均值', group_sum - avg_target)
-        print(u'共', group_num, u'个数值，全部完成均分')
+    # 判断readygroup是否达成目标，达成则break
+    if ready_group_num == val_list_len:
+        print(ready_group)
+        group_list.append(ready_group)
+        print('---单组合计', ready_group_sum, '相差均值', ready_group_sum - avg_target)
+        print('共', ready_group_num, '个数值，全部完成均分')
         break
-    elif abs(avg_target - group_sum) < min(val_queue):
-        print(group)
-        group_list.append(group)
-        print(u'--单组合计', group_sum, u'相差均值', group_sum - avg_target)
-        group = []
-    elif group_sum >= avg_target:
-        print(group)
-        group_list.append(group)
-        print(u'-单组合计', group_sum, u'相差均值', group_sum - avg_target)
-        group = []
+    elif abs(avg_target - ready_group_sum) < min(val_list_queue):
+        print(ready_group)
+        group_list.append(ready_group)
+        print('--单组合计', ready_group_sum, '相差均值', ready_group_sum - avg_target)
+        ready_group = []
+    elif ready_group_sum >= avg_target:
+        print(ready_group)
+        group_list.append(ready_group)
+        print('-单组合计', ready_group_sum, '相差均值', ready_group_sum - avg_target)
+        ready_group = []
 
-    if len(val_queue) == 1:
-        cur = val_queue.popleft()
-        group.append(cur)
-        group_num += 1
-        print(group)
-        group_list.append(group)
-        print(u'---单组合计', sum(group), u'相差均值', sum(group) - avg_target)
-        print(u'\n共', group_num, u'个数值，全部完成均分')
+    if len(val_list_queue) == 1:
+        cur = val_list_queue.popleft()
+        ready_group.append(cur)
+        ready_group_num += 1
+        print(ready_group)
+        group_list.append(ready_group)
+        print('---单组合计', sum(ready_group), '相差均值', sum(ready_group) - avg_target)
+        print('\n共', ready_group_num, '个数值，全部完成均分')
         break
     else:
-        cur = val_queue.popleft()
+        cur = val_list_queue.popleft()
         if cur >= avg_target:
-            group.append(cur)
-            group_num += 1
-            print(group)
-            group_list.append(group)
-            print(u'大于均值 相差均值', sum(group) - avg_target)
-            group = []
+            ready_group.append(cur)
+            ready_group_num += 1
+            print(ready_group)
+            group_list.append(ready_group)
+            print('大于均值 相差均值', sum(ready_group) - avg_target)
+            ready_group = []
             continue
         else:
-            curx = abs(avg_target - group_sum - cur)
-            oxlist = [abs(avg_target - group_sum - o) for o in val_queue]
+            curx = abs(avg_target - ready_group_sum - cur)
+            oxlist = [abs(avg_target - ready_group_sum - o) for o in val_list_queue]
             ox = min(oxlist)
             if curx <= ox:
-                group.append(cur)
-                group_num += 1
+                ready_group.append(cur)
+                ready_group_num += 1
             else:
-                val_queue.append(cur)
+                val_list_queue.append(cur)
 
-print(u'\n----以下为对应id的分组----\n')
-bids_list = deque(bids_list)
+print('\n----以下为对应id的分组----')
+id_val_list = deque(id_val_list)
 
 wresult = open('result.txt', 'w')
 
 for gl in group_list:
-    bid_group = []
+    id_group = []
     for gi in gl:
         while True:
-            bidque = bids_list.popleft()
-            if gi != bidque[1]:
-                bids_list.append(bidque)
+            idque = id_val_list.popleft()
+            if gi != idque[1]:
+                id_val_list.append(idque)
             else:
-                bid_group.append(int(bidque[0]))
+                id_group.append(int(idque[0]))
                 break
-    print(bid_group)
-    wresult.write(str(bid_group))
+    print(id_group)
+    wresult.write(str(id_group))
     wresult.write('\n')
 
 wresult.close()
 
-print('*' * 75)
+print('*' * 78)
 anyenter = input("Grouped completed.Press Enter to quit.")
 sys.exit()
